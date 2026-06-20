51930e2480b8752d1ad959039ff83cdc81e25d6f
Qunerum <qunerumofficial@gmail.com>
2026-06-20 10:20:21 +0200

    update 2026-06-20 10:20

--- src/main.c
+++ src/main.c
@@ -9,6 +9,6 @@ int main() {
 	setLogs(1);
 	setCmdPrefix('$');
 	addCommand("help", _help);
-	madeBot("MTUxNzIxMTkyNDU1Mzc5NzgzMw.GQ54WB.4WPuZhri2tXTZ4xCgdThFB9Gh4A2VTGI6thWh4", initFn);
+	madeBot("YOUR_DISCORD_BOT_TOKEN", initFn);
 	return 0;
 }
